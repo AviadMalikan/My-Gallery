fd885d2a1231220c2c5dd328042c7a5a3b0a8bd6
AviadMaliakn <aviad879@gmail.com>
2022-12-05 20:16:11 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@ console.log('Starting up');
 const GITHUB_URL = `https://aviadmalikan.github.io/`
 const gProjects = [
     {
+        
         id: "sokoban",
         name: "Sokoban",
         title: "Better push those boxes",
         desc: "lorem ipsum lorem ipsum lorem ipsum",
-        url: GITHUB_URL + "Sokoban/",
+        url: GITHUB_URL + "Sokobon/",
         publishedAt: 1668067200,
         labels: ["Matrixes", "keyboard events"],
     },
@@ -23,7 +24,7 @@ const gProjects = [
     {
         id: "pacman",
         name: "Pacman",
-        title: "Collect those ball",
+        title: "Give me power!",
         desc: `Why doesn't Pacman use Instagram? He doesn't like followers`,
         url: GITHUB_URL + "Ball-Board/",
         publishedAt: 1668931200,
@@ -49,40 +50,75 @@ const gProjects = [
     },
     {
         id: "books-shop",
-        name: "Booking",
-        title: "Hotel ? No its book shop",
+        name: "Book shop",
+        title: "Know booking? so not. just regular shop",
         desc: "i born with a reading list i will never finish",
-        url: GITHUB_URL + "Task-web/",
+        url: GITHUB_URL + "Book-shop/",
         publishedAt: 1669795200,
         labels: ["Matrixes", "keyboard events"],
     },
 ]
 
 
-// $().click(openModal())
+$(document).ready(init)
 
-renderPortfolio()
+
+function init() {
+    renderPortfolio()
+}
 
 function renderPortfolio() {
-    console.log('HEY')
-
     var strHTMLs = gProjects.map(proj => `
         <div class="col-md-4 col-sm-6 portfolio-item">
-        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1" onclick="openModal('${proj.id}')">
           <div class="portfolio-hover">
             <div class="portfolio-hover-content">
               <i class="fa fa-plus fa-3x"></i>
             </div>
           </div>
-          <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
+          <img class="h-100 w-100" src="img/portfolio/${proj.id}.png" alt="">
         </a>
-        <div class="portfolio-caption">
+        <div class="portfolio-caption ${proj.id}">
           <h4>${proj.name}</h4>
           <p class="text-muted">${proj.title}</p>
         </div>
       </div>`)
-      console.log('strHTMLs: ', strHTMLs)
-      
+
     var strHTML = strHTMLs.join('')
     $('.portfolio-container').html(strHTML)
+}
+
+function openModal(projId) {
+    console.log('projId: ', projId)
+
+    console.log('OLA')
+    var proj = gProjects.find(project => project.id === projId)
+    var time = new Date(proj.publishedAt)
+    console.log('time: ', time)
+
+    var day = time.getDay()
+    var month = time.getMonth() + 1
+    var year = time.getFullYear()
+
+    console.log('month: ', month)
+
+
+
+    var strHTML = `
+    <h2>${proj.name}</h2>
+    <p class="item-intro text-muted">${proj.title}</p>
+    <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.id}.png" alt="">
+    <p>${proj.desc}</p>
+    <ul class="list-inline">
+    <li>Date: ${day}/${month}/${year}</li>
+    <li>Url: <a href="${proj.url}"> Click here to see</a></li>
+    <li>Category :  ${proj.labels}</li>
+    </ul>
+    <button class="btn btn-primary" data-dismiss="modal" type="button">
+    <i class="fa fa-times"></i>
+    Close Project</button>
+    </div>`;
+
+
+    $('.modal-body').html(strHTML)
 }
